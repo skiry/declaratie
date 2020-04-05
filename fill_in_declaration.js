8e179493fc5e3cf7f56f7bb16470cfaa2e4a6dd6
Skiry <farauanuionut@gmail.com>
2020-04-05 13:15:20 +0300

    Check fetch api

--- fill_in_declaration.js
+++ fill_in_declaration.js
@@ -1,5 +1,23 @@
-function generate_pdf() {
-	console.log("running")
-	var element = document.getElementById('element-to-print');
-	html2pdf(element);
+modifyPdf();
+async function modifyPdf() {
+  const url = './decl2.pdf';
+  const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+
+  const pdfDoc = await PDFDocument.load(existingPdfBytes)
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+  const pages = pdfDoc.getPages()
+  const firstPage = pages[0]
+  const { width, height } = firstPage.getSize()
+  firstPage.drawText('STRICT SECRET!', {
+    x: 5,
+    y: height / 2 + 300,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(-45),
+  })
+
+  const pdfBytes = await pdfDoc.save()
+  download(pdfBytes, "pdf-lib_modification_example.pdf", "application/pdf");
 }
