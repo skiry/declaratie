47b40d0f55dae238fce57b03220a3aedd00515dc
Skiry <farauanuionut@gmail.com>
2020-04-06 16:19:49 +0300

    Check lengths

--- pdfWriter.js
+++ pdfWriter.js
@@ -69,17 +69,32 @@ async function modifyPdf() {
 	};
 
 	const main = async function () {
-	  const url = './declaratie-buna.pdf'
-	  const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+	  const url = './declaratie-buna.pdf';
+	  const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
 
 	  // Load a PDFDocument from the existing PDF bytes
-	  const pdfDoc = await PDFDocument.load(existingPdfBytes)
+	  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+	  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+	  const pages = pdfDoc.getPages();
+  	  const firstPage = pages[0];
+
 	  const acroForm = getAcroForm(pdfDoc);
 	  acroForm.set(PDFName.of('NeedAppearances'), PDFBool.True);
 
+	  var fields = ["nume", "prenume", "deplasare"];
+	  var locations = [[10, 30], [50, 100], [100, 150]];
+	  var sizes = [12.4, 12.4, 14];
+
+	  for (let i = 0; i < fields.length; ++i) {
+		let value = document.getElementsByName(fields[i])[0].value;
+		firstPage.drawText(value, {
+	    x: locations[i][0],
+	    y: locations[i][1],
+	    size: sizes[1],
+	    font: helveticaFont
+	  })
+	}
 	  fillInRadioButton(pdfDoc, 'Check Box3', 'Check Box1');
-	  fillInField(pdfDoc, 'nume', x.elements[0].value);
-	  fillInField(pdfDoc, 'prenume', x.elements[1].value);
 	  logAcroFieldNames(pdfDoc);
 
 	  const pdfBytes = await pdfDoc.save();
@@ -87,7 +102,30 @@ async function modifyPdf() {
 	  // Trigger the browser to download the PDF document
 	  download(pdfBytes, "pdf-lib_modification_example.pdf", "application/pdf");
 	 }
-	 
+
 	main();	
 }
 	
+
+/*
+//TODO
+-draw on the pdf, along with writing the values, for portability -if one wants to change his file-
+-fix stuff with checkboxes - take care if i bifate 1 and 2, stuff...
+
+ var mycanvas = document.getElementById("signature-pad"); //get your canvas
+var image = mycanvas.toDataURL("image/png"); //Convert
+document.getElementById("semnatura_hidden").src = image;
+image = image.replace('data:image/png;base64,', '');
+document.getElementById("semnatura_hidden").value = image;
+
+doc.addImage(image, "PNG", 125, 170, 50, 25);
+
+thats how they do it
+
+
+helvetica - data 19.6
+deplasare - 14
+restul - 12.4
+
+*/
+
